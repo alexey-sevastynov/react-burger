a8125bcd1cfc09e34ca0c38c3c9dd12c23464f18
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-04-30 12:03:46 +0300

    created components basket

---
+++ src/components/basket/BasketEmpty.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import iconEmtyBasket from "../../assets/img/empty-cart.png";
+import { Link } from "react-router-dom";
+
+function BasketEmpty() {
+  return (
+    <div className="cart cart--empty">
+      <h2>Basket empty !</h2>
+      <p>
+        You probably haven't ordered pizza yet.
+        <br />
+        To order pizza, go to the main page.
+      </p>
+      <img src={iconEmtyBasket} alt="Empty cart" />
+      <Link to="/" className="button button--black">
+        <span>Come back</span>
+      </Link>
+    </div>
+  );
+}
+
+export default BasketEmpty;
--- src/page/BasketPage.jsx
+++ src/page/BasketPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import BasketEmpty from "../components/basket/BasketEmpty";
+import BasketItem from "../components/basket/BasketItem";
 
 function BasketPage() {
-  return <div>BasketPage</div>;
+  return (
+    <div className="content">
+      <div className="container container--cart">
+        {/* <BasketItem /> */}
+        <BasketEmpty />
+      </div>
+    </div>
+  );
 }
 
 export default BasketPage;
