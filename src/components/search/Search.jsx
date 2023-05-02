8e2b4c68d721ebca5cbb022a725feca40bce2100
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-05-02 15:35:11 +0300

    useRef focus()

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -7,12 +7,20 @@ import { Context } from "../../App";
 
 function Search() {
   const { searchValue, setSearchValue } = React.useContext(Context);
+
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue("");
+    inputRef.current.focus();
+  };
+
   return (
     <div className={styles.root}>
       <img className={styles.search} src={search} alt="search" />
       {searchValue && (
         <img
-          onClick={() => setSearchValue("")}
+          onClick={onClickClear}
           className={styles.close}
           src={close}
           alt="close"
@@ -20,6 +28,7 @@ function Search() {
       )}
 
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(event) => setSearchValue(event.target.value)}
         placeholder="search burger..."
